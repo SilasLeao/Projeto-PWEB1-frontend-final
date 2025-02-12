01829552f3a333dc78bacbcfcbb43d3f47fc917b
SilasLeao <leaosilas@gmail.com>
2025-02-12 08:42:49 -0300

    .

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -5,6 +5,8 @@ import { NewsService } from '../../services/news.service';
 import { CommonModule } from '@angular/common';
 import {MatButton} from '@angular/material/button';
 
+
+// Componente para listagem de notícias.
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
@@ -16,21 +18,25 @@ import {MatButton} from '@angular/material/button';
     MatButton
   ]
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent {
+
+  // Lista de notícias recebida como entrada do componente pai.
   @Input() newsList: News[] = [];
 
+  // Construtor do componente, injeta o serviço de notícias (métodos getNews e updateLikesDislikes) para interagir com o db.json .
   constructor(private newsService: NewsService) {}
 
-  ngOnInit(): void {}
-
+  // Alterna o estado de expansão da notícia para mostrar/ocultar o texto completo.
   toggleExpand(news: News) {
     news.expanded = !news.expanded;
   }
 
+  // Converte quebras de linha em '<br>' para exibição correta no HTML.
   convertNewlinesToBreaks(text: string): string {
     return text.replace(/\n/g, '<br>');
   }
 
+  // Incrementa o número de "likes" de uma postagem e atualiza no JSON-Server.
   likePost(news: News) {
     news.like++;
     this.newsService.updateLikesDislikes(news).subscribe({
@@ -40,6 +46,7 @@ export class PostListComponent implements OnInit {
     });
   }
 
+  // Incrementa o número de "dislikes" de uma postagem e atualiza no JSON-Server.
   dislikePost(news: News) {
     news.dislike++;
     this.newsService.updateLikesDislikes(news).subscribe({
@@ -47,6 +54,5 @@ export class PostListComponent implements OnInit {
         console.error('Erro ao atualizar o dislike:', err);
       }
     });
-    console.log()
   }
 }
